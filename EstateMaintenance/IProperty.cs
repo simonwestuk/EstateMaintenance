994de9ed77fdf199a406ff848b91f096a9e6a407
wests <wests@C-010053>
2022-04-26 11:16:30 +0100

    Add project files.

---
+++ EstateMaintenance/IProperty.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EstateMaintenance
+{
+    internal interface IProperty
+    {
+        public string Address { get; set; }
+
+    }
+}
